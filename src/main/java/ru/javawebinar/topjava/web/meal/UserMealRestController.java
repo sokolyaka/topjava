713efe32bcd914431691b6d3ad335d0260ff1073
sokolyaka <sokolov.ob@gmail.com>
2016-08-28 22:37:20 +0300

    HW07 added UserMealRestControllerTest

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -1,22 +1,60 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.UserMeal;
-import ru.javawebinar.topjava.util.TimeUtil;
+import ru.javawebinar.topjava.to.UserMealWithExceed;
 
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Objects;
+import java.net.URI;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static ru.javawebinar.topjava.web.meal.UserMealRestController.REST_URL;
 
 /**
  * GKislin
  * 06.03.2015.
  */
-@Controller
+@RestController
+@RequestMapping(REST_URL)
 public class UserMealRestController extends AbstractUserMealController {
+    static final String REST_URL = "/rest/meals";
+
+
+    @Override
+    @RequestMapping(method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    public List<UserMealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    public UserMeal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = APPLICATION_JSON_VALUE)
+    public void update(@RequestBody UserMeal userMeal, @PathVariable("id") int id) {
+        super.update(userMeal, id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserMeal> createOne(@RequestBody UserMeal userMeal) {
+        UserMeal created = super.create(userMeal);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @Override
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
 }
---
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
@@ -0,0 +1,86 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.service.UserMealService;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+
+public class UserMealRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = UserMealRestController.REST_URL + '/';
+
+    @Autowired
+    private UserMealService mealService;
+
+
+    @Test
+    public void getAll() throws Exception {
+        mockMvc.perform(get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void getOne() throws Exception {
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentMatcher(MEAL1));
+    }
+
+    @Test
+    public void update() throws Exception {
+        UserMeal updated = getUpdated();
+        mockMvc
+                .perform(put(REST_URL + MEAL1_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        UserMeal actual = mealService.get(updated.getId(), AuthorizedUser.id());
+        MATCHER.assertEquals(updated, actual);
+    }
+
+    @Test
+    public void create() throws Exception {
+        UserMeal created = getCreated();
+        ResultActions actions = mockMvc
+                .perform(post(REST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtil.writeValue(created)))
+                .andExpect(status().isCreated());
+
+        UserMeal returned = MATCHER.fromJsonAction(actions);
+        created.setId(returned.getId());
+
+        MATCHER.assertEquals(created, returned);
+        MATCHER.assertCollectionEquals(Arrays.asList(created, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1), mealService.getAll(USER_ID));
+    }
+
+    @Test
+    public void deleteOne() throws Exception {
+        mockMvc.perform(delete(REST_URL + MEAL1_ID))
+                .andExpect(status().isOk());
+        MATCHER.assertCollectionEquals(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2), mealService.getAll(USER_ID));
+    }
+
+
+}
