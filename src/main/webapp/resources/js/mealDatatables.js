a1aab4239af970cf69bdb548df1331d141113ad2
Oleg Sokolov <sokolov.ob@gmail.com>
2016-09-14 11:02:24 +0300

    meal list to datatable ajax

---
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -0,0 +1,70 @@
+var ajaxUrl = 'ajax/profile/meals/';
+var datatableApi;
+
+function updateTable() {
+    $.ajax({
+        type: "POST",
+        url: ajaxUrl + 'filter',
+        data: $('#filter').serialize(),
+        success: updateTableByData
+    });
+    return false;
+}
+
+$(function () {
+    datatableApi = $('#datatable').DataTable({
+        "ajax": {
+            "url": ajaxUrl,
+            "dataSrc": ""
+        },
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "dateTime",
+                "render": function (date, type, row) {
+                    if (type == 'display') {
+                        var dateObject = new Date(date);
+                        return '<span>' + dateObject.toISOString().substring(0, 10) + '</span>';
+                    }
+                    return date;
+                }
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderEditBtn
+            },
+            {
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderDeleteBtn
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            if (data.exceed) {
+                $(row).addClass('exceeded');
+            } else {
+                $(row).addClass('normal');
+            }
+        },
+        "initComplete": makeEditable
+    });
+
+    $('#filter').submit(function () {
+        updateTable();
+        return false;
+    });
+});
