053446ac44042d8cf42b30b765ebc8c8f827d91f
Oleg Sokolov <sokolov.ob@gmail.com>
2016-10-10 17:24:13 +0300

    10 10 controller based exception

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealAjaxController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.to.UserMealWithExceed;
+import ru.javawebinar.topjava.web.ExceptionInfoHandler;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -19,7 +20,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(value = "/ajax/profile/meals")
-public class UserMealAjaxController extends AbstractUserMealController {
+public class UserMealAjaxController extends AbstractUserMealController implements ExceptionInfoHandler {
 
 /*
   Other validation solution:
@@ -49,6 +50,7 @@ public class UserMealAjaxController extends AbstractUserMealController {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<String> updateOrCreate(@Valid UserMeal meal, BindingResult result) {
         if (result.hasErrors()) {
+            // TODO change to exception handler
             StringBuilder sb = new StringBuilder();
             result.getFieldErrors().forEach(fe -> sb.append(fe.getField()).append(" ").append(fe.getDefaultMessage()).append("<br>"));
             return new ResponseEntity<>(sb.toString(), HttpStatus.UNPROCESSABLE_ENTITY);
